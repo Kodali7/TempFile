172eb579ccf87454082a39912a548f7b345f27d6
Kodali7 <kodali.sai07@gmail.com>
2025-08-30 16:40:27 -0500

    Created extension downloading logic

--- popup.js
+++ popup.js
@@ -1,1 +1,27 @@
-console.log("Hello")
+chrome.downloads.onCreated.addListener(function(downloadItem) {
+    console.log("Download Started")
+    handleDownloads(downloadItem);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("slider")
+    
+    toggleButton.addEventListener("change", function() {
+        if (toggleButton.checked) { //check if slider is activated
+            console.log("Switch is ON");
+        } else { //return to downloading normally
+            console.log("Switch is OFF");
+        }
+    });
+});
+
+chrome.downloads.onChanged.addListener(function(downloadDelta) {
+    if((downloadDelta.state && downloadDelta.state.current === "complete")) {
+        console.log("Download completed");
+    }
+});
+
+function handleDownloads(downloadItem) {
+    var fileurl = downloadItem.fileUrl;
+    chrome.downloads.download({url: fileurl, filename: "temp/" + downloadItem.filename});
+}
